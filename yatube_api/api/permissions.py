1bf2d596ab7c002d08d035fc0483f0c01664f09c
Maksim Zhovna <m.zhovna@1440.space>
2025-07-14 00:32:30 +0300

    fix: удалены неиспользуемые комментарии и улучшен стиль кода в файлах permissions.py, serializers.py и urls.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,17 +2,13 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """Разрешает чтение всем, а изменение — только автору объекта."""
-
     def has_permission(self, request, view):
-        # На уровне запроса: писать можно только будучи залогиненным
         return (
             request.method in permissions.SAFE_METHODS
             or request.user and request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        # На уровне объекта: только автор
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
